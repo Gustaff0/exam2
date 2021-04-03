160e7a804a888c8b4b497946078af4e0f5194c67
Bars <barsbekclash@gmail.com>
2021-04-03 12:19:49 +0600

    add models and register in admin, add fixtures

--- poll/webapp/admin.py
+++ poll/webapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from webapp.models import Poll, Choice
 
 # Register your models here.
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question', 'created_at']
+    list_filter = ['created_at', 'question']
+    search_fields = ['question']
+    fields = ['id', 'question']
+    readonly_fields = ['created_at','id']
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'choice_text', 'poll']
+    list_filter = ['poll']
+    search_fields = ['choice_text']
+    fields = ['id', 'choice_text', 'poll']
+    readonly_fields = ['id']
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- poll/webapp/models.py
+++ poll/webapp/models.py
@@ -3,4 +3,26 @@ from django.db import models
 # Create your models here.
 
 class Poll(models.Model):
+    question = models.CharField(max_length=400, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'Poll'
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы'
+
+    def __str__(self):
+        return f'{self.question} - {self.created_at}'
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=400, blank=False, null=False)
+    poll = models.ForeignKey('webapp.Poll', on_delete=models.CASCADE, related_name='poll', verbose_name='Опрос')
+
+    class Meta:
+        db_table = 'Choice'
+        verbose_name = 'Вариант'
+        verbose_name_plural = 'Варианты'
+
+    def __str__(self):
+        return f'{self.choice_text} - {self.poll}'
 
